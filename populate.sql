8fb4a646d900bd05a8815b27e5de1a2eb1f4d3e7
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-24 23:43:34 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -7,16 +7,16 @@ INSERT INTO crime (crime_name) VALUES ('murder');
 INSERT INTO crime (crime_name) VALUES ('theft');
 INSERT INTO crime (crime_name) VALUES ('beating');
 
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bob','12-10-75', 'yes', 'M', 1);
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boba','10-10-84', 'no', 'F', 2);
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boban','11-09-90', 'yes', 'M', 3);
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boby','09-10-90', 'yes', 'M', 4);
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bobych','08-10-94', 'no', 'F', 5);
-INSERT INTO person ( name, date_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bib','06-10-74', 'yes', 'M', 6);
+INSERT INTO person ( name, year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bob',1974, 'yes', 'M', 1);
+INSERT INTO person ( name, year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boba',1999, 'no', 'F', 2);
+INSERT INTO person ( name,  year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boban',1990, 'yes', 'M', 3);
+INSERT INTO person ( name,  year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Boby',1992, 'yes', 'M', 4);
+INSERT INTO person ( name,  year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bobych',1982, 'no', 'F', 5);
+INSERT INTO person ( name,  year_of_birth, armed_yes_or_no, gender, person_id) VALUES ('Bib',1990, 'yes', 'M', 6);
 
-INSERT INTO criminal (date_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES ('10-04-17', 1, 'murder', 'Lviv',1);
-INSERT INTO criminal (date_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES ('10-05-15', 2,  'theft', 'Rum',2);
-INSERT INTO criminal (date_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES ('11-02-16', 3, 'beating', 'Krakiv',3);
+INSERT INTO criminal (year_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES (2017, 1, 'murder', 'Lviv',1);
+INSERT INTO criminal (year_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES (2019, 2,  'theft', 'Rum',2);
+INSERT INTO criminal (year_of_crime, crime_id,  crime_crime_name, city_city_name, person_person_id) VALUES (2011, 3, 'beating', 'Krakiv',3);
 
 INSERT INTO policeman (person_person_id, policeman_id) VALUES (4, 4);
 INSERT INTO policeman (person_person_id, policeman_id) VALUES (5, 5);
