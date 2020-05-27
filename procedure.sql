42042c9822680faa03a8a0cdecd317419886324b
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-27 12:48:52 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -16,25 +16,32 @@ BEGIN
     SELECT COUNT(*)
     INTO indeficate_id
     FROM police
-    WHERE
-        policeman_person_person_id LIKE policeman_policeman_id;
+    WHERE policeman_person_person_id LIKE policeman_policeman_id
+    ;
 
     SELECT COUNT(*)
     INTO indeficate_status
     FROM police 
-    WHERE status='solved';
+    WHERE status='solved'
+    ;
 
     IF (indeficate_id = 1) and (indeficate_status = 1)  THEN
-      INSERT INTO police (criminal_crime_id,criminal_person_person_id, status, policeman_person_person_id, policeman_policeman_id)   VALUES (1, 7, 'solved', 4, 4);
+      INSERT INTO police (criminal_crime_id,criminal_person_person_id, status, policeman_person_person_id, policeman_policeman_id)   
+        VALUES (1, 2, 'solved', 4, 4);
 
-    ELSE
-        RAISE no_data;
+    ELSE RAISE no_data;
 
-    END IF;
+    END IF
+;
+
 
 EXCEPTION
     WHEN no_data THEN
-        dbms_output.put_line('No such person or crime in the database');
+        dbms_output.put_line('No such person or crime in the database')
+    ;
+    
     WHEN OTHERS THEN
-        dbms_output.put_line('Wrong ');
+        dbms_output.put_line('Wrong')
+    ;
+    
 END;
