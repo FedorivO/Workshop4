0ac22b1eaaa18fd86d2153465fb6d444798ae8fa
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-27 12:45:58 +0300

    Add files via upload

---
+++ procedure.sql
@@ -0,0 +1,40 @@
+create or replace PROCEDURE add_crime (
+    policeman_policeman_id      number
+    ,
+    policeman_person_person_id    number
+    ,
+    status   VARCHAR
+) AS
+    indeficate_id NUMBER
+    ;
+    indeficate_status NUMBER
+    ;
+    no_data EXCEPTION
+    ;
+BEGIN
+
+    SELECT COUNT(*)
+    INTO indeficate_id
+    FROM police
+    WHERE
+        policeman_person_person_id LIKE policeman_policeman_id;
+
+    SELECT COUNT(*)
+    INTO indeficate_status
+    FROM police 
+    WHERE status='solved';
+
+    IF (indeficate_id = 1) and (indeficate_status = 1)  THEN
+      INSERT INTO police (criminal_crime_id,criminal_person_person_id, status, policeman_person_person_id, policeman_policeman_id)   VALUES (1, 7, 'solved', 4, 4);
+
+    ELSE
+        RAISE no_data;
+
+    END IF;
+
+EXCEPTION
+    WHEN no_data THEN
+        dbms_output.put_line('No such person or crime in the database');
+    WHEN OTHERS THEN
+        dbms_output.put_line('Wrong ');
+END;
